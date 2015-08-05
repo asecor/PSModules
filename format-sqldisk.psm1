161b5ff9e5f0927093c5bad03b06a2783ae47417
asecor <asecor@users.noreply.github.com>
2015-08-05 19:33:49 +0200

    PSModules
    First upload of PowerShell modules that I have put together.

---
+++ format-sqldisk.psm1
@@ -0,0 +1,8 @@
+﻿function Format-SQLDisk{
+[cmdletBinding()]
+param(
+    [Parameter(Mandatory=$True)]
+    [string[]]$computername
+)
+Invoke-Command -ComputerName $computername -ScriptBlock {get-disk | where PartitionStyle -eq "raw" | Initialize-Disk -PartitionStyle GPT -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -AllocationUnitSize 65536 -NewFileSystemLabel "SQLDATA" -Force -Confirm:$false}
+}
---
+++ install-rdsbasic.psm1
@@ -0,0 +1,11 @@
+﻿
+function Install-RDSBasic{
+    [cmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True)]
+        [string[]]$computername
+        )
+    Invoke-Command -ComputerName $computername -ScriptBlock {Import-Module RemoteDesktop | Install-WindowsFeature –Name RDS-RD-Server | Out-Null | Set-RDLicenseConfiguration -LicenseServer RDSLICENCESERVER -Mode PerDevice -Force | Restart-Computer}
+}
+
+#$obj = gwmi -namespace "Root/CIMV2/TerminalServices" Win32_TerminalServiceSetting ; $obj.ChangeMode(2); $obj.SetSpecifiedLicenseServerList("RDSLICENCESERVER")
