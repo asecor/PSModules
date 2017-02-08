75d0cd7b31b4bc1a18528eb827cd8723d6ac449d
Corin Asembrener <nokitel@gmail.com>
2017-02-08 20:33:27 +0100

    First commit

---
+++ Get-VDisk.psm1
@@ -0,0 +1,20 @@
+function Get-VDisk {
+    [CmdletBinding()]
+    param (
+        [string]$ComputerName
+    )
+    
+    begin {
+        $TotalGB = @{Name="Capacity(GB)";expression={[math]::round(($_.Size/ 1073741824),2)}}
+        $FreeGB = @{Name="FreeSpace(GB)";expression={[math]::round(($_.FreeSpace / 1073741824),2)}}
+        $FreePerc = @{Name="Free(%)";expression={[math]::round(((($_.FreeSpace / 1073741824)/($_.Size / 1073741824)) * 100),0)}}
+    }
+    
+    process {
+        $volumes = Get-WmiObject -computer $ComputerName win32_logicaldisk | Where-object {$_.Size -ne $null}
+        $volumes | Select-Object SystemName, DeviceID, $TotalGB, $FreeGB, $FreePerc | Format-Table -AutoSize
+    }
+    
+    end {
+    }
+}
