c6b938abcdb415f9696427fd92a456f0a131a5dd
Corin Asembrener <nokitel@gmail.com>
2017-04-19 09:54:42 +0200

    Update Add-xLocalAdmin.ps1

--- Add-xLocalAdmin.ps1
+++ Add-xLocalAdmin.ps1
@@ -2,7 +2,7 @@
 Invoke-Command -ComputerName server1 -ScriptBlock {net localgroup Administrators "domain\username or group" /add}
 
 #post Windows Server 2016
-#Invoke-Command -ComputerName server1 -ScriptBlock {Add-LocalGroupMember -Group "Administrators" -Member "domain\username or group"}
+Invoke-Command -ComputerName server1 -ScriptBlock {Add-LocalGroupMember -Group "Administrators" -Member "domain\username or group"}
 
 #to add some logic determening system then execute relevant command
 #get user input
